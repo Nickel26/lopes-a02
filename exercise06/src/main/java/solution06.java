4db80abf5e3a41e7e52503c3457da834eb9d4a81
Nick <lopes.nick0801@gmail.com>
2021-09-15 12:36:42 -0400

    fix

--- exercise06/src/main/java/solution06.java
+++ exercise06/src/main/java/solution06.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class solution06 {
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Nicholas Lopes
+ */
+
+public class solution06 {
+    public static void main(String[] args){
+        System.out.printf("Hello World!");
+    }
+
 }
