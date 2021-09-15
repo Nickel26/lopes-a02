b1afb4b0dc3752694682e9b94ee745156ff9b53b
Nick <lopes.nick0801@gmail.com>
2021-09-15 13:40:37 -0400

    Project commit

--- exercise07/src/main/java/Solution07.java
+++ exercise07/src/main/java/Solution07.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class solution07 {
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Nicholas Lopes
+ */
+public class Solution07 {
+    private static final Scanner in = new Scanner(System.in);
+    public static void main(String[] args){
+
+    }
+
 }
